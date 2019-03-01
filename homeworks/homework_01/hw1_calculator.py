ecca6e4bbf0101d3c7ea1429b9554638a08ff9cf
andrew <voroninandrewi@gmail.com>
2019-03-02 01:28:27 +0300

    Add hw1_calculator.py solution

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -10,4 +10,15 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
-    raise NotImplementedError
+    if (y == 0) and (operator == 'divide'):
+        return None
+    elif (type(x) not in [int, float]) or (type(y) not in [int, float]):
+        return None
+    elif (operator == 'plus'):
+        return x + y
+    elif (operator == 'minus'):
+        return x - y
+    elif (operator == 'mult'):
+        return x * y
+    elif (operator == 'divide'):
+        return x / y
