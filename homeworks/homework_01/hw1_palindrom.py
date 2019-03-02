9a2688c5b06059cee9e21e2bbb501d08e263b9b4
andrew <voroninandrewi@gmail.com>
2019-03-02 23:18:32 +0300

    Add hw1_palindrom.py solution

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,4 +10,4 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
-    raise NotImplementedError
+    return True if (input_string == input_string[::-1]) else False
