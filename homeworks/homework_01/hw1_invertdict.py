56c222f8d4c9769a9d7e460a43268cecdb265bae
andrew <voroninandrewi@gmail.com>
2019-03-03 15:31:23 +0300

    Add hw1_invertdict.py solution

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -9,4 +9,39 @@ def invert_dict(source_dict):
     :param source_dict: dict
     :return: new_dict: dict
     '''
-    raise NotImplementedError
+    if (type(source_dict) == dict):
+
+        new_dict = {}
+
+        def unpack(values):
+            list_of_values = []
+
+            if (type(values) not in [list, tuple, set]):
+                return [values]
+
+            else:
+                for value in values:
+                    list_of_values += unpack(value)
+            return list_of_values
+
+        for (key, values) in source_dict.items():
+            list_of_values = unpack(values)
+            source_dict[key] = list_of_values
+
+        for (key, values) in source_dict.items():
+            for value in values:
+
+                if (value not in new_dict.keys()):
+                    new_dict[value] = key
+                else:
+                    key_list = []
+
+                    if (type(new_dict[value]) != list):
+                        key_list.append(new_dict[value])
+                    else:
+                        key_list += new_dict[value]
+
+                    key_list.append(key)
+                    new_dict[value] = key_list
+
+        return new_dict
