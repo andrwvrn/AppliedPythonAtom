53d08200fea8133ec03ea50f91951cbd8c4e089b
andrew <voroninandrewi@gmail.com>
2019-03-17 17:49:32 +0300

    Add hw3_lrucache.py solution

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,55 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import collections
+import time
 
-class LRUCacheDecorator:
 
-    def __init__(self, maxsize, ttl):
-        '''
-        :param maxsize: максимальный размер кеша
-        :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+def LRUCacheDecorator(maxsize=None, ttl=None):
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    class LRUCacheDecClass:
+
+        def __init__(self, func):
+            '''
+            :param maxsize: максимальный размер кеша
+            :param ttl: время в млсек, через которое кеш
+                        должен исчезнуть
+            '''
+            # TODO инициализация декоратора
+            #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
+            self._size = 0
+            self._ttl = ttl
+            self._cache = collections.OrderedDict()
+            self._func = func
+            self._t_end = None
+
+        def __call__(self, *args, **kwargs):
+            # TODO вызов функции
+            if (self._ttl and self._t_end and
+                    time.time() - self._t_end >= self._ttl):
+                self._cache.clear()
+                self._size = 0
+
+            key = args
+            kw = None
+            if (kwargs):
+                kw = sorted(kwargs.items())
+                key = args, *kw
+
+            if (not self._cache.get(key)):
+                if (self._size != maxsize):
+                    self._cache[key] = self._func(*args, **kwargs)
+                    self._size += 1
+                else:
+                    self._cache.popitem(last=False)
+                    self._cache[key] = self._func(*args, **kwargs)
+
+                self._t_end = time.time()
+                return self._cache[key]
+
+            else:
+                self._cache[key] = self._cache.pop(key)
+                self._t_end = time.time()
+                return self._cache[key]
+
+    return LRUCacheDecClass
