b34cb763e770193a94726df55db51d04cbfc641b
andrew <voroninandrewi@gmail.com>
2019-03-24 01:08:26 +0300

    Add hw4_wordcounter.py solution

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
+import functools
 import os
+
+
+res_dict = Manager().dict()
+
+
+def read_file(filename, pth):
+    with open(pth + '/' + filename) as f:
+        res_dict[filename] = len(f.read().split())
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +25,11 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    p = Pool()
+    list_of_files = os.listdir(path_to_dir)
+    p.map(functools.partial(read_file, pth=path_to_dir), list_of_files)
+
+    p.close()
+    p.join()
+    res_dict['total'] = sum(res_dict.values())
+    return res_dict
