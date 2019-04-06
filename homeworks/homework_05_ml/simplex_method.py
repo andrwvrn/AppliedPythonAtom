c66812bd7a0e6e18cae9f22bb7e04841a6c7fb4f
andrew <voroninandrewi@gmail.com>
2019-04-07 00:42:39 +0300

    Add simplex_method.py solution

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,24 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+
+    new_a = np.vstack((a, -c))
+    simplex_tab = np.hstack((new_a, np.eye(b.shape[0]+1), np.append(b, 0)[:, None]))
+
+    res = np.zeros((c.shape[0]))
+    while np.any(simplex_tab[-1] < 0):
+        ind_col = np.argmin(simplex_tab[-1])
+
+        with np.errstate(divide='ignore'):
+            ind_row = np.argmin(np.divide(simplex_tab[:-1, -1],
+                                          simplex_tab[:-1, ind_col]))
+
+        simplex_tab[ind_row] = simplex_tab[ind_row]/simplex_tab[ind_row, ind_col]
+        new_tab = simplex_tab - simplex_tab[ind_row]*simplex_tab[:, ind_col, None]
+        new_tab[ind_row] = simplex_tab[ind_row]
+        simplex_tab = new_tab
+
+    x_ind = np.where(simplex_tab[:-1, :c.shape[0]] == 1)
+    res[x_ind[1]] = simplex_tab[:-1, -1][x_ind[0]]
+
+    return res
