9e938af1a4ad4e2ea01b3d31c3018aaa6db36e93
andrew <voroninandrewi@gmail.com>
2019-04-15 20:10:02 +0300

    Add metrics.py solution

--- homeworks/homework_06_ml/metrics.py
+++ homeworks/homework_06_ml/metrics.py
@@ -12,7 +12,7 @@ def mse(y_true, y_hat, derivative=False):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    return np.mean((y_true-y_hat)**2)
 
 
 def mae(y_true, y_hat):
@@ -22,7 +22,7 @@ def mae(y_true, y_hat):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    return np.mean(np.absolute(y_true-y_hat))
 
 
 def r2_score(y_true, y_hat):
@@ -32,4 +32,4 @@ def r2_score(y_true, y_hat):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    return (1 - np.sum((y_true-y_hat)**2)/np.sum((y_true-np.mean(y_true))**2))
