569219125757c8d677ee08d888b1f24638bbb56c
andrew <voroninandrewi@gmail.com>
2019-04-26 16:14:20 +0300

    Minor fixes

--- homeworks/homework_07_ml/logreg/logistic_regression.py
+++ homeworks/homework_07_ml/logreg/logistic_regression.py
@@ -13,23 +13,23 @@ class NotFittedError(Exception):
 
 
 class LogisticRegression:
-    def __init__(self, lambda_coef=0.5, regularization=None, alpha=0.5):
+    def __init__(self, lambda_coef=0.5, regularization=None, alpha=0.5, n_iters=100000):
         """
         LogReg for Binary case
         :param lambda_coef: constant coef for gradient descent step
         :param regulatization: regularizarion type ("L1" or "L2") or None
         :param alpha: regularizarion coefficent
         """
-        self._lambda_coef = lambda_coef
-
         if (regularization not in ("L1", "L2", None)):
             raise ValueError("regularization parameter must be 'L1', 'L2' or None.")
 
+        self._lambda_coef = lambda_coef
+        self._n_iters = n_iters
         self._reg = regularization
         self._alpha = alpha
         self._coef = None
 
-    def fit(self, X_train, y_train, delta=0.00001, n_iters=100000, coef_rand_seed=222):
+    def fit(self, X_train, y_train, delta=0.00001, coef_rand_seed=222):
         """
         Fit model using gradient descent method
         :param X_train: training data
@@ -59,7 +59,7 @@ class LogisticRegression:
             prev = None
             reg_term = 0
 
-            for _ in range(n_iters):
+            for _ in range(self._n_iters):
                 if (prev is not None and np.all(np.absolute(self._coef - prev) < delta)):
                     break
                 else:
@@ -81,7 +81,7 @@ class LogisticRegression:
             for i in zero_std:
                 self._coef = np.insert(self._coef, i, np.zeros(k), axis=0)
 
-    def predict(self, X_test, threshold=0.5):
+    def predict(self, X_test):
         """
         Predict using model.
         :param X_test: test data for predict in
