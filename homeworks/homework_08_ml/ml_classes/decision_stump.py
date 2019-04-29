adfbdf600ff3372a92e7dd567c3fc8b4c37a9f6a
andrew <voroninandrewi@gmail.com>
2019-04-29 15:36:33 +0300

    Add decision_stump.py solution

--- homeworks/homework_08_ml/ml_classes/decision_stump.py
+++ homeworks/homework_08_ml/ml_classes/decision_stump.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+
+
+class NotFittedError(Exception):
+    def __init__(self):
+        message = "Data is not fitted yet. Call 'fit' method before using this method."
+        Exception.__init__(self, message)
+
 
 class DecisionStumpRegressor:
     '''
@@ -13,7 +21,9 @@ class DecisionStumpRegressor:
         Мы должны создать поля, чтобы сохранять наш порог th и ответы для
         x <= th и x > th
         '''
-        raise NotImplementedError
+        self._th = np.array([])
+        self._pred = np.array([])
+        self._mse = np.array([])
 
     def fit(self, X, y):
         '''
@@ -22,7 +32,26 @@ class DecisionStumpRegressor:
         :param y: целевая переменная (1, num_objects)
         :return: None
         '''
-        raise NotImplementedError
+        X_train = X.flatten()
+        y_train = y.flatten()
+
+        for x in X_train:
+            left_child_ = y_train[X_train <= x]
+            right_child_ = y_train[X_train > x]
+
+            mse_left = np.sum((left_child_ - np.mean(left_child_))**2)
+            if len(right_child_ > 0):
+                mse_right = np.sum((right_child_ - np.mean(right_child_))**2)
+            else:
+                mse_right = 0
+
+            mse_ = mse_left + mse_right
+            self._th = np.append(self._th, x)
+            self._mse = np.append(self._mse, mse_)
+
+        self._best_th = self._th[np.argmin(self._mse)]
+        self._pred = np.append(self._pred, np.mean(y_train[X_train <= self._best_th]))
+        self._pred = np.append(self._pred, np.mean(y_train[X_train > self._best_th]))
 
     def predict(self, X):
         '''
@@ -30,4 +59,12 @@ class DecisionStumpRegressor:
         :param X: массив размера (1, num_objects)
         :return: массив, размера (1, num_objects)
         '''
-        raise NotImplementedError
+        if len(self._mse > 0):
+            y_pred = np.zeros(X.shape)
+            y_pred[X <= self._best_th] = self._pred[0]
+            y_pred[X > self._best_th] = self._pred[1]
+
+            return y_pred.flatten()
+
+        else:
+            raise NotFittedError
